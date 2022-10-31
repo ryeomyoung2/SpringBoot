53515809348270a4541ca1b248558bc0d0fae020
ryeomyoung2 <ryeomyoung2@gmail.com>
2022-10-31 17:13:56 +0900

    Files: Updated

---
+++ Ex10_ValidationUtils2/src/test/java/com/study/springboot/Ex10ValidationUtils2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex10ValidationUtils2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
