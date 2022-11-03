db408b544c5c79d5cab6253460d29c7da0992e2a
ryeomyoung2 <ryeomyoung2@gmail.com>
2022-11-03 16:58:51 +0900

    Files: Updated

---
+++ Ex13_Jdbc-1/src/main/java/com/study/springboot/Ex13Jdbc1Application.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex13Jdbc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex13Jdbc1Application.class, args);
+	}
+
+}
