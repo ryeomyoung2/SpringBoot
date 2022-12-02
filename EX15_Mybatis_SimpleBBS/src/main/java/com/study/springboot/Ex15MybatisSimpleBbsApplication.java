900fa16260092f36cea13a6734851600f8dbdca7
ryeomyoung2 <ryeomyoung2@gmail.com>
2022-12-02 12:47:28 +0900

    updated

---
+++ EX15_Mybatis_SimpleBBS/src/main/java/com/study/springboot/Ex15MybatisSimpleBbsApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex15MybatisSimpleBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex15MybatisSimpleBbsApplication.class, args);
+	}
+
+}
