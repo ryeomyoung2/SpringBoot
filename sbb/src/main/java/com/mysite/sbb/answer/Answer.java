dd863b67d5c4504e000fcd11e4b0306bdd57ec62
ryeomyoung2 <ryeomyoung2@gmail.com>
2022-12-15 14:57:59 +0900

    updated

--- sbb/src/main/java/com/mysite/sbb/answer/Answer.java
+++ sbb/src/main/java/com/mysite/sbb/answer/Answer.java
@@ -1,16 +1,19 @@
 package com.mysite.sbb.answer;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import org.springframework.data.annotation.CreatedDate;
 
 import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,4 +34,12 @@ public class Answer {
 
     @ManyToOne
     private Question question;
+    
+    @ManyToOne
+    private SiteUser author;
+    
+    private LocalDateTime modifyDate;
+    
+    @ManyToMany
+    Set<SiteUser> voter;
 }
